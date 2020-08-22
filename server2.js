68c3f444dcc873138a61314fa28ebd684d552de7
Cormac Donohoe <57358706+cormac0108@users.noreply.github.com>
2020-08-23 00:12:02 +0100

    Intro to Express.js

---
+++ server2.js
@@ -0,0 +1,6 @@
+// using express 23rd of August.  npm install express
+const express = require('express')
+const app = express();
+
+app.listen(3002);
+// express has a greaqt guide to getting started. 
