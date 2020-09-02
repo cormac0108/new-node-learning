16ea8660274299536428d5fdfe2228f51b05be6a
Cormac Donohoe <57358706+cormac0108@users.noreply.github.com>
2020-09-02 21:06:45 +0100

    Node server for face-recognition-app

--- server.js
+++ server.js
@@ -1,57 +1,99 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex')
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'aneagoie',
+    password : '',
+    database : 'smart-brain'
+  }
+});
 
 const app = express();
+
+app.use(cors())
 app.use(bodyParser.json());
 
-const database = { 
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cook123',
-            entries: 0,
+app.get('/', (req, res)=> {
+  res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+  db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db.select('*').from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            res.json(user[0])
+          })
+          .catch(err => res.status(400).json('unable to get user'))
+      } else {
+        res.status(400).json('wrong credentials')
+      }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
+})
+
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
             joined: new Date()
-        }, 
-        {
-            id: '1234',
-            name: 'Sam',
-            email: 'Sam@gmail.com',
-            password: 'bandana',
-            entries: 0,
-            joined: new Date() 
-        }
-    ]
-}
-
-// create a basic route. 
-app.get('/', (req, res) => {
-    res.send('this is working! ');
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json('unable to register'))
 })
 
-//remember to parse the json using body-parsr.
-
-app.post('/signin', (req , res) => {
-    // res.send('signing -hello')
-    // res.json('signing')
-    if (req.body.email === database.users[0].email && 
-        req.body.password === database.users[0].password) {
-        res.json('success');
-    } else {
-        res.status(400).json('error logging in');
-    }
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  db.select('*').from('users').where({id})
+    .then(user => {
+      if (user.length) {
+        res.json(user[0])
+      } else {
+        res.status(400).json('Not found')
+      }
+    })
+    .catch(err => res.status(400).json('error getting user'))
 })
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0]);
+  })
+  .catch(err => res.status(400).json('unable to get entries'))
 })
 
-/* Our projects endpoint layout 
-/ --> res = this is working
-/sigin --> POST = success/fail
-/register --> POST = user
-/profile/:userId --> GET = user
-/image --> PUT --> user
-We will test them with postman to make sure they are working. 
-*/
+app.listen(3000, ()=> {
+  console.log('app is running on port 3000');
+})
